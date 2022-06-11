3b5d9d22b05e5195ee51f7facd535ad752b29c52
Serdar Durdyyev <se.durdyev@gmail.com>
2022-06-11 13:03:33 +0200

    changes

--- My project (2)/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
+++ My project (2)/Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/InputField.cs
@@ -194,6 +194,12 @@ namespace UnityEngine.UI
         protected TouchScreenKeyboard m_Keyboard;
         static private readonly char[] kSeparators = { ' ', '.', ',', '\t', '\r', '\n' };
 
+    #if UNITY_ANDROID
+        static private bool s_IsQuestDeviceEvaluated = false;
+    #endif // if UNITY_ANDROID
+
+        static private bool s_IsQuestDevice = false;
+
         /// <summary>
         /// Text Text used to display the input's value.
         /// </summary>
@@ -321,6 +327,7 @@ namespace UnityEngine.UI
 
         // Doesn't include dot and @ on purpose! See usage for details.
         const string kEmailSpecialCharacters = "!#$%&'*+-/=?^_`{|}~";
+        const string kOculusQuestDeviceModel = "Oculus Quest";
 
         protected InputField()
         {
@@ -1036,6 +1043,21 @@ namespace UnityEngine.UI
 
     #endif // if UNITY_EDITOR
 
+    #if UNITY_ANDROID
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (s_IsQuestDeviceEvaluated)
+                return;
+
+            // Used for Oculus Quest 1 and 2 software keyboard regression.
+            // TouchScreenKeyboard.isInPlaceEditingAllowed is always returning true in these devices and would prevent the software keyboard from showing up if that value was used.
+            s_IsQuestDevice = SystemInfo.deviceModel == kOculusQuestDeviceModel;
+            s_IsQuestDeviceEvaluated = true;
+        }
+    #endif // if UNITY_ANDROID
+
         protected override void OnEnable()
         {
             base.OnEnable();
@@ -1246,9 +1268,33 @@ namespace UnityEngine.UI
             }
         }
 
+        // Returns true if the TouchScreenKeyboard should be used. On Android and Chrome OS, we only want to use the
+        // TouchScreenKeyboard if in-place editing is not allowed (i.e. when we do not have a hardware keyboard available).
+        private bool TouchScreenKeyboardShouldBeUsed()
+        {
+            RuntimePlatform platform = Application.platform;
+            switch (platform)
+            {
+                case RuntimePlatform.Android:
+                    if (s_IsQuestDevice)
+                        return TouchScreenKeyboard.isSupported;
+
+                    return !TouchScreenKeyboard.isInPlaceEditingAllowed;
+                default:
+                    return TouchScreenKeyboard.isSupported;
+            }
+        }
+
         private bool InPlaceEditing()
         {
             return !TouchScreenKeyboard.isSupported || m_TouchKeyboardAllowsInPlaceEditing;
+        }
+
+        // In-place editing can change state if a hardware keyboard becomes available or is hidden while the input field is activated.
+        // This currently only happens on Chrome OS devices (that support laptop and tablet mode).
+        private bool InPlaceEditingChanged()
+        {
+            return !s_IsQuestDevice && m_TouchKeyboardAllowsInPlaceEditing != TouchScreenKeyboard.isInPlaceEditingAllowed;
         }
 
         void UpdateCaretFromKeyboard()
@@ -1301,6 +1347,21 @@ namespace UnityEngine.UI
             }
 
             AssignPositioningIfNeeded();
+
+            // If the device's state changed in a way that affects whether we should use a touchscreen keyboard or not,
+            // then we make sure to clear all of the caret/highlight state visually and deactivate the input field.
+            if (isFocused && InPlaceEditingChanged())
+            {
+                if (m_CachedInputRenderer != null)
+                {
+                    using (var helper = new VertexHelper())
+                        helper.FillMesh(mesh);
+
+                    m_CachedInputRenderer.SetMesh(mesh);
+                }
+
+                DeactivateInputField();
+            }
 
             if (!isFocused || InPlaceEditing())
                 return;
@@ -2508,7 +2569,7 @@ namespace UnityEngine.UI
                 return;
 #endif
             // No need to draw a cursor on mobile as its handled by the devices keyboard.
-            if (!shouldHideMobileInput)
+            if (!InPlaceEditing() && !shouldHideMobileInput)
                 return;
 
             if (m_CachedInputRenderer == null && m_TextComponent != null)
@@ -2897,7 +2958,12 @@ namespace UnityEngine.UI
             if (EventSystem.current.currentSelectedGameObject != gameObject)
                 EventSystem.current.SetSelectedGameObject(gameObject);
 
-            if (TouchScreenKeyboard.isSupported)
+            // Cache the value of isInPlaceEditingAllowed, because on UWP this involves calling into native code
+            // Usually, the value only needs to be updated once when the TouchKeyboard is opened; however, on Chrome OS,
+            // we check repeatedly to see if the in-place editing state has changed, so we can take action.
+            m_TouchKeyboardAllowsInPlaceEditing = !s_IsQuestDevice && TouchScreenKeyboard.isInPlaceEditingAllowed;
+
+            if (TouchScreenKeyboardShouldBeUsed())
             {
                 if (input != null && input.touchSupported)
                 {
@@ -2906,10 +2972,6 @@ namespace UnityEngine.UI
                 m_Keyboard = (inputType == InputType.Password) ?
                     TouchScreenKeyboard.Open(m_Text, keyboardType, false, multiLine, true, false, "", characterLimit) :
                     TouchScreenKeyboard.Open(m_Text, keyboardType, inputType == InputType.AutoCorrect, multiLine, false, false, "", characterLimit);
-
-                // Cache the value of isInPlaceEditingAllowed, because on UWP this involves calling into native code
-                // The value only needs to be updated once when the TouchKeyboard is opened.
-                m_TouchKeyboardAllowsInPlaceEditing = TouchScreenKeyboard.isInPlaceEditingAllowed;
 
                 // If TouchKeyboard doesn't support InPlaceEditing don't call OnFocus as mobile doesn't properly support select all
                 // Just set it to the end of the text (where it would move when typing starts)
