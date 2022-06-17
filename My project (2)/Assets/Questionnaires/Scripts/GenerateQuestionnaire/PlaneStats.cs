9dcb00fe9edd95346f430e523736a12245c805fc
Serdar Durdyyev <se.durdyev@gmail.com>
2022-06-17 16:49:38 +0200

    texture adjustment

--- My project (2)/Assets/Questionnaires/Scripts/GenerateQuestionnaire/PlaneStats.cs
+++ My project (2)/Assets/Questionnaires/Scripts/GenerateQuestionnaire/PlaneStats.cs
@@ -21,6 +21,16 @@ namespace VRQuestionnaireToolkit
         public static int firedEvent = 0;
         public static string currentQuestionnaireId = "";
 
+        //Balanced Latin Square
+        /*  p1  A   B   H   C   G   D   F   E 
+            p2	B	C	A	D	H	E	G	F
+            p3	C	D	B	E	A	F	H	G
+            p4	D	E	C	F	B	G	A	H
+            p5	E	F	D	G	C	H	B	A
+            p6	F	G	E	H	D	A	C	B
+            p7	G	H	F	A	E	B	D	C
+            p8	H	A	G	B	F	C	E	D
+        */
         public static int[] vr_scene_order_4_p1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
         public static int[] vr_scene_order_4_p2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
         public static int[] vr_scene_order_4_p3 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
@@ -29,6 +39,8 @@ namespace VRQuestionnaireToolkit
         public static int[] vr_scene_order_4_p6 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
         public static int[] vr_scene_order_4_p7 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
         public static int[] vr_scene_order_4_p8 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
+
+    
 
 
         //Set corresponding array for each participant
